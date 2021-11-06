915457fc99efd05830b65a0690c5f1725b1020e2
Ayanprakash35 <74096403+Ayanprakash35@users.noreply.github.com>
2021-11-06 12:56:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,51 +1,50 @@
-img = "";
-status = "";
-objects = [];
-objectDetector = "";
-function setup(){
-    canvas = createCanvas(640, 420);
-    canvas.center();
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status - detecting objects";
-}
+objectDetector= "";
 
 img = "";
-status = "";
 objects = [];
+status = "";
+
 function preload(){
-    img = loadImage('bedroom.jpg');
+  img = loadImage('bedroom.jpg');
 }
 
-function draw(){
-    image(img, 0, 0, 640, 420);
-    if(status !="")
-    {
-        for (i = 0; i <objects.lenght; i++){
-            document.getElementById("status").innerHTML = "Status : Object Detected";
 
-            fill(255,0,0);
-            percent = floor(object[i].confidence * 100);
-            text(objects[i].label + " "+ percent + "%", objects[i].x + 15, objects[i].y + 15);
-            noFill();
-            stroke(255,0,0);
-            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+
+function draw() {
+  image(img, 0, 0, 640, 420);
+
+      if(status != "")
+      {
+        for (var i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+   
+          fill(255, 0, 0);
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+          noFill();
+          stroke(255, 0, 0);
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         }
-    }
-}
-
-function modelLoaded(){
-    console.log('modelLoaded');
-    status = true;
-    objectDetector.detect(img, gotResults);
-}
-
-function gotResults(error, results){
-    if(error){
-        console.log(error);
-
-    }
-    else{
-        console.log(results);
-        objects = results;
-    }
+      }
 }
